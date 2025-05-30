8d9fa85b4072af97a609a437ee234cfcc65ed4e8
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2025-05-30 10:37:46 -0300

    passo2
    Co-authored-by: Pedro Gonçalves Costa Melo <Pedro-Goncalves-Costa-Melo@users.noreply.github.com>

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.Random;
 
-public abstract class Animal {
+public abstract class Animal implements Ator {
 
     // Características compartilhadas por todos os coelhos (atributos estáticos, da classe).
 
@@ -114,7 +114,7 @@ public abstract class Animal {
      * Novos nascimentos serão feitos em locais vizinhos livres.
      * @param novosAnimais Uma lista para retornar os animais recém-nascidos.
      */
-    protected void reproduzir(List<Animal> novosAnimais)
+    protected void reproduzir(List<Ator> novosAnimais)
     {
         // Novos animais nascem em locais vizinhos.
         // Obtém uma lista de locais vizinhos livres.
@@ -136,8 +136,7 @@ public abstract class Animal {
         return obterIdade() >= obterIdadeReproducao();
     }
 
-    
-    public abstract void agir(List<Animal> novosAnimais);
+    public abstract void agir(List<Ator> novosAnimais);
 
     protected abstract int obterIdadeMaxima();
     
--- src/VisaoDeTexto.java
+++ src/VisaoDeTexto.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 
 public class VisaoDeTexto implements VisaoSimulador {
